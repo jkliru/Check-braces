9bf68dbd4bfbdbcc6c0dbddccd150e2f94d34488
S.Cheglakov <loxmatwj@yandex.ru>
2018-08-08 18:53:57 +0300

    Initial commit

---
+++ src/CheckBraces.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace CheckBraces;
+
+/**
+ * Проверяет корректность открытых/закрытых круглых скобок
+ */
+class CheckBraces
+{
+    public function isCorrectString($str) {
+        if (preg_match('/[^\(\)\t\r\n ]/', $str))
+            throw new InvalidArgumentException('Unexpected symbol in a string');
+
+        $braces = [];
+
+        foreach(str_split($str) as $index => $symbol) {
+            switch($symbol) {
+                case '(':
+                    array_push($braces, '(');
+                    break;
+                case ')':
+                    if (empty($braces)) {
+                        return false;
+                    }
+
+                    array_pop($braces);
+                    break;
+            }
+        }
+
+        if (!empty($braces))
+            return false;
+
+        return true;
+    }
+}
